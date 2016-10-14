2521d64d8163db515c6d239db3967f986580f4df
ideasiii <louisju@iii.org.tw>
2016-10-14 16:42:48 +0800

    update

--- MoreJava/src/sdk/ideas/Mongo.java
+++ MoreJava/src/sdk/ideas/Mongo.java
@@ -63,10 +63,37 @@ public class Mongo
 		}
 	}
 
+	/**
+	 * 
+	 * @param strDB
+	 * @param strCollection
+	 * @param listFilter
+	 * @param listResult
+	 * @return
+	 * @example Mongo mongo = new Mongo(); mongo.Connect("localhost", 27017);
+	 *          ArrayList<String> listResult = new ArrayList<String>();
+	 *          ArrayList<Filter> listFilter = new ArrayList<Filter>();
+	 * 
+	 *          Filter f1 = new Filter(); f1.strField = "ID";
+	 *          f1.mapFilter.put("$regex", strAPPID); listFilter.add(f1);
+	 * 
+	 *          if (null != strStartDate || null != strEndDate) { Filter f2 =
+	 *          new Filter(); f2.strField = "create_date"; if (null !=
+	 *          strStartDate) { f2.mapFilter.put("$gte", strStartDate + "
+	 *          00:00:00"); }
+	 * 
+	 *          if (null != strEndDate) { f2.mapFilter.put("$lte", strEndDate +
+	 *          " 23:59:59"); } listFilter.add(f2); }
+	 * 
+	 *          int nCount = mongo.query("access", "mobile", listFilter,
+	 *          listResult);
+	 */
 	@SuppressWarnings("deprecation")
 	public int query(final String strDB, final String strCollection, final ArrayList<Filter> listFilter,
 			ArrayList<String> listResult)
 	{
+		if (null == mongo)
+			return -1;
 		DB db = mongo.getDB(strDB);
 		if (null != db)
 		{
@@ -112,6 +139,52 @@ public class Mongo
 		}
 
 		return listResult.size();
+	}
+
+	@SuppressWarnings("deprecation")
+	public int insert(final String strDB, final String strCollection)
+	{
+		try
+		{
+			// get database
+			// if database doesn't exists, mongodb will create it for you
+			DB db = mongo.getDB(strDB);
+
+			// get collection
+			// if collection doesn't exists, mongodb will create it for you
+			DBCollection collection = db.getCollection(strCollection);
+
+			/**** Insert ****/
+			// create a document to store key and value
+
+			BasicDBObject document;
+			String address[];
+			for (int i = 0; i < array_names.length; i++)
+			{
+				document = new BasicDBObject();
+				// value -> String
+				document.append("name", array_names[i]);
+				// value -> int
+				document.append("age", (int) (Math.random() * 60));
+				// value -> date
+				document.append("join", new Date());
+				// value -> array
+				document.append("friends", pickFriends());
+
+				address = pickAddress();
+				// value --> document
+				document.append("address", new BasicDBObject("country", address[0]).append("state", address[1])
+						.append("city", address[2]));
+
+				collection.insert(document);
+
+			}
+
+		} catch (Exception e)
+		{
+
+		}
+		return 0;
 	}
 
 }
